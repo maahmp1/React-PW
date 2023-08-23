f713db9ef46e81a6053ced465332e65aa6a72668
Maahmp1 <Marcelamartinspena@gmail.com>
2023-08-23 08:56:57 -0300

    update: cardapio index.tsx and styles

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -5,7 +5,16 @@ export default function Cardapio(){
     <main>
         <nav className={ styles.menu}>
             <img src={Logo} alt="logo" />
+            
         </nav>
+        <header className={styles.header}>
+        <div className={styles.header__text}>
+            "A casa do código e da massa!"
+        </div>
+        </header>
+        <section>
+            <h3>Cardápio</h3>
+        </section>
     </main>
     );
 }
